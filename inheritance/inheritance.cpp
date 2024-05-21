2d37de3fd682cb27fcd72c6a07d3b202c02b06c7
Weellyyy <wildanprtm2304@gmail.com>
2024-05-21 20:57:52 +0700

    mengganti nama sma

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -35,7 +35,7 @@ public:
 };
 
 int main() {
-	pelajar siswa1("andi laksono", "SMAN ! BANTUL");
+	pelajar siswa1("andi laksono", "SMAN 1 BANTUL");
 	cout << siswa1.perkenalan();
 	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
 
